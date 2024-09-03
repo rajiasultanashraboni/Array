3cd50ff521efc3b2254c4fa5dccb75e883ea198d
rajia sultana shraboni <shraboni3103@gmail.com>
2024-09-03 22:29:30 +0600

    second task is completed here

---
+++ tasktwo.js
@@ -0,0 +1,6 @@
+let touristDestinations = ["Paris", "Tokyo", "New York"];
+touristDestinations.push("Sydney");
+touristDestinations.push("London", "Rome");
+touristDestinations.pop(); // Removes "Rome"
+console.log(touristDestinations);
+
